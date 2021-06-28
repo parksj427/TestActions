224a26d327c40d038455f1a6d945540d45b08ae9
Seongjun Park <75301534+parksj427@users.noreply.github.com>
2021-06-28 16:47:04 +0900

    Update Program.cs

--- TestActions/TestActions/Program.cs
+++ TestActions/TestActions/Program.cs
@@ -10,7 +10,7 @@ namespace TestAction1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Console.ReadLine();
+            Console.ReadLine(); 
 
         }
     }
